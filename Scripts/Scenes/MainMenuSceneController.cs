28508960c8c3b29e38c5d76f42ee3b70471044c6
Alexander <alex.dllsky@gmail.com>
2018-04-12 10:52:48 +0300

    Update MainMenuSceneController.cs

--- Scripts/Scenes/MainMenuSceneController.cs
+++ Scripts/Scenes/MainMenuSceneController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//test git
+
 public class MainMenuSceneController : Singleton<MainMenuSceneController>
 {
     #region Variables
