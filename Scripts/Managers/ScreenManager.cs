38c855a18f710307e4ebc838c36047c4ab72edca
Alexander Danilovsky <alex.dllsky@gmail.com>
2018-04-12 17:20:20 +0300

    Changed ScreenController

--- Scripts/Managers/ScreenManager.cs
+++ Scripts/Managers/ScreenManager.cs
@@ -35,7 +35,58 @@ public class ScreenManager : Singleton<ScreenManager>
         var screen = Instantiate(ResourcesManager.LoadPrefab(ConstantsResourcesPath.SCREENS, _name), parent);
         screen.transform.SetAsLastSibling();
         screen.GetComponent<ScreenController>().Initialize(_data);
+
+        /*
+        var screen = ResourcesManager.Instance.InstantiatePrefab<ScreenController>(transform, ResourcesManagerPaths.UI_SCREENS, _type.ToString());
+        screens.Add(screen);
+        screen.Type = _type;
+        screen.Initialize(_data);
+
+        LocalizationManager.ClearCache();
+
+        return screen;
+        */
     }
+
+    public DialogController ShowDialog(string _name)
+    {
+        return ShowDialog<DialogController>(_name);
+    }
+
+    public DialogController ShowErrorDialog(string _text, string _techInfo = null)
+    {
+        if (screens.Count > 0)
+            _text += "\n" + LocalizationManager.Get(LocalizationKeys.ERROR_INFO_SCREEN, screens[screens.Count - 1].Type.ToString());
+
+        if (_techInfo == null)
+            _techInfo = Environment.StackTrace;
+
+        var dialog = ShowDialog<ErrorDialogController>(DialogNameConstants.ERROR, AlwaysOverlayCanvas);
+        dialog.Initialize(_text, _techInfo);
+        return dialog;
+    }
+    /*
+    /// <summary>
+    /// Creates dialog either in current screen or in plain canvas if no current screen.
+    /// </summary>
+    public T ShowDialog<T>(string _name) where T : DialogController
+    {
+        if (screens.Count > 0)
+            return ShowDialog<T>(_name, screens[screens.Count - 1]);
+        else
+            return ShowDialog<T>(_name, this);
+    }
+
+    public T ShowDialog<T>(string _name, MonoBehaviour _parent) where T : DialogController
+    {
+        return ResourcesManager.Instance.InstantiatePrefab<T>(_parent.transform, ResourcesManagerPaths.UI_DIALOGS, _name);
+    }
+
+    public T ShowDialog<T>(string _name, Transform _transform) where T : DialogController
+    {
+        return ResourcesManager.Instance.InstantiatePrefab<T>(_transform, ResourcesManagerPaths.UI_DIALOGS, _name);
+    }
+    */
     #endregion
 
     #region Private methods
