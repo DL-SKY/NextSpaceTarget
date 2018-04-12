df159d333b78bb5ec1e8c26094eb203ec9d32c48
Alexander Danilovsky <alex.dllsky@gmail.com>
2018-04-12 16:25:38 +0300

    Add ToolsMain

---
+++ Scripts/Managers/ToolsMain.cs
@@ -0,0 +1,25 @@
+﻿using DllSky.Patterns;
+using DllSky.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsMain : Singleton<ToolsMain>
+{
+    #region Variables
+    public bool defaultFlag = false;
+    #endregion
+
+    #region Unity methods    
+    #endregion
+
+    #region Public methods
+    #endregion
+
+    #region Private methods
+    #endregion
+
+    #region Coroutines
+    #endregion
+}
+
