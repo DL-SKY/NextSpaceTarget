fa2f64ea31b24a8d73470fa07fa25f62860418b1
Alexander Danilovsky <alex.dllsky@gmail.com>
2018-04-13 12:55:25 +0300

    new

--- Scripts/DllSky/Utility/ScrollRectItemsScaler.cs
+++ Scripts/DllSky/Utility/ScrollRectItemsScaler.cs
@@ -1,28 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScrollRectItemsScaler : MonoBehaviour
+namespace DllSky.Utility
 {
-    #region Vars
-    public float multiplyer = 1;
-
-	ScrollRect scrollRect;
-	#endregion
-
-	#region Unity methods
-	void Start()
+    [RequireComponent(typeof(ScrollRect))]
+    public class ScrollRectItemsScaler : MonoBehaviour
     {
-        scrollRect = GetComponent<ScrollRect>();
-	}
+        #region Vars
+        public float multiplyer = 1;
 
-	void Update()
-	{
-        for (int i = 0; i < scrollRect.content.childCount; i++)
+        ScrollRect scrollRect;
+        #endregion
+
+        #region Unity methods
+        void Start()
         {
-            var child = scrollRect.content.GetChild (i);
-            float dist = (child.transform.position - scrollRect.transform.position).magnitude;
-            child.transform.localScale = new Vector3 (1, 1, 1) * (1 - dist / Screen.width * multiplyer);
-		}
+            scrollRect = GetComponent<ScrollRect>();
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < scrollRect.content.childCount; i++)
+            {
+                var child = scrollRect.content.GetChild(i);
+                float dist = (child.transform.position - scrollRect.transform.position).magnitude;
+                child.transform.localScale = new Vector3(1, 1, 1) * (1 - dist / Screen.width * multiplyer);
+            }
+        }
+        #endregion
     }
-    #endregion
 }
